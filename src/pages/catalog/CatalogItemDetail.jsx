9d91b9e4b6034f573f543eb43c29a20c02f57d33
amondrave <amondrave54@gmail.com>
2025-12-27 01:24:48 -0500

    🚧 amondrave feat: add reviews section

--- src/pages/catalog/CatalogItemDetail.jsx
+++ src/pages/catalog/CatalogItemDetail.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import avatar_usuario from '../../assets/images/user.png';
 
 export default function CatalogItemDetail() {
     const { book } = useLocation().state;
@@ -21,7 +22,6 @@ export default function CatalogItemDetail() {
     }
     return (
         <div className="container-detail">
-
             <div className="container-detail-image">
                 <img src={url_caratula} alt={titulo} />
             </div>
@@ -39,6 +39,25 @@ export default function CatalogItemDetail() {
                 </button>
                 <div className="alert alert-info" role="alert">
                     <p className="m-0"><b>Entrega Digital:</b> Recibe el acceso instantáneo tras la compra.</p>
+                </div>
+            </div>
+            <div className="separator"></div>
+            <div className="container-detail-reviews">
+                <h4>Reseñas de la comunidad ⭐</h4>
+                <div className="bg-body-tertiary p-4 rounded border">
+                    <h6>Escribir una reseña</h6>
+                    <textarea className="form-control mt-2 mb-2" placeholder="Escribe tu reseña aquí"></textarea>
+                    <button className="btn btn-outline-dark">
+                        Publicar reseña
+                    </button>
+                </div>
+                <div className="d-flex gap-2">
+                    {/* <img className="rounded-circle" src={avatar_usuario} alt="" style={{width: '40px', height: '40px'}} /> */}
+                    <div className="bg-body-secondary p-3 rounded-circle border" style={{width: '40px', height: '40px'}}></div>
+                    <div>
+                        <h6>Usuario123 <span className="text-muted" style={{fontSize: '0.75rem', fontWeight: 'normal'}}>• Hace 2 días</span></h6>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+                    </div>
                 </div>
             </div>
         </div>
